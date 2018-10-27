f87a09675f6d1ab33288d126b1a1098112acfeb9
juanengml <juanengml@gmail.com>
2018-10-27 20:30:49 -0300

    Update

--- lcdedsonip.py
+++ lcdedsonip.py
@@ -5,7 +5,7 @@ import time,os # importa biblioteca de tempo e os para sistema operacional
 Lcd = lcd.Jhd1313m1(0, 0x3E, 0x62) # Cria uma variavel para usar o display
 Lcd.clear()  # Limpa tela
 Lcd.setColor(255, 255, 0)  # Seta cor verde
-Lcd.setCursor(0,0) # Inicia o Cursor na posição x = 0,y = 0
+Lcd.setCursor(0,0) # Inicia o Cursor na posicao x = 0,y = 0
 
 while True:
  ip = netools.local_ip()

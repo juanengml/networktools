e63970a6e5f709b0303c3e306ee90db0df6fa9a5
juanengml <juanengml@gmail.com>
2018-10-27 20:41:42 -0300

    Update

--- lcdedsonip.py
+++ lcdedsonip.py
@@ -5,9 +5,22 @@ import time,os # importa biblioteca de tempo e os para sistema operacional
 Lcd = lcd.Jhd1313m1(0, 0x3E, 0x62) # Cria uma variavel para usar o display
 Lcd.clear()  # Limpa tela
 Lcd.setColor(255, 255, 0)  # Seta cor verde
-Lcd.setCursor(0,0) # Inicia o Cursor na posicao x = 0,y = 0
+
+def msg(name,ip):
+    Lcd.clear()
+    Lcd.setCursor(0,0) # Inicia o Cursor na posicao x = 0,y = 0
+    Lcd.write(name)
+    Lcd.setCursor(0,1) # Inicia o Cursor na posicao x = 0,y = 0
+    Lcd.write(ip)
 
 while True:
  ip = netools.local_ip()
- Lcd.write(ip)  # escreve no LCD o IP
- time.sleep(0.5) # Espera d
+ iot0 = netools.ping("Pi0","192.168.100.10")
+ msg("Edson",ip)
+ time.sleep(2)
+ msg(iot0[0],iot0[1])
+ time.sleep(2)
+
+
+
+

654e9a19d46e3bc11cf36dda9c91ba2f9a8a4527
juanengml <juanengml@gmail.com>
2018-10-27 20:51:25 -0300

    Update

--- ip.py
+++ ip.py
@@ -16,15 +16,17 @@ SPI_DEVICE = 0
 disp = LCD.PCD8544(DC, RST, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=4000000))
 disp.begin(contrast=40)
 disp.clear()
+image = Image.new('1', (LCD.LCDWIDTH, LCD.LCDHEIGHT))
+draw = ImageDraw.Draw(image)
+draw.rectangle((0,0,LCD.LCDWIDTH,LCD.LCDHEIGHT), outline=255, fill=255)
+font = ImageFont.load_default()
+ 
 
-def msg(text):
+def msg(name,ip):
  disp.clear()
  disp.display()
- image = Image.new('1', (LCD.LCDWIDTH, LCD.LCDHEIGHT))
- draw = ImageDraw.Draw(image)
- draw.rectangle((0,0,LCD.LCDWIDTH,LCD.LCDHEIGHT), outline=255, fill=255)
- font = ImageFont.load_default()
- draw.text((1,10), text, font=font)
+ draw.text((1,10), name, font=font)
+ draw.text((1,20), ip, font=font)
  disp.image(image)
  disp.display()
 
@@ -32,6 +34,11 @@ def msg(text):
 while  True:
  print('Press Ctrl-C to quit.')
  ip = netools.local_ip()
- time.sleep(1)
- msg(ip)
+ iot1 = netools.ping("Edson","192.168.100.13")
+ msg("Pi0",ip)
+ time.sleep(2)
+ msg(iot1[0],iot1[1])
+ time.sleep(2)
+ 
+
  
